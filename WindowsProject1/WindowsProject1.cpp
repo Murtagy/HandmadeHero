dc82bae88f8858b0b72ba21ed75355e957bd5ae9
Murtagy <mrabkevich.gmail.com>
2023-12-05 22:21:30 +0300

    bitmap

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -1,8 +1,60 @@
 ﻿#include <windows.h>
 // #include <iostream>
 
+#define internal static
+#define local_persist static
+#define global_variable static
 
-LRESULT CALLBACK MainWindowCallback(
+
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+
+internal void
+WinResizeDIBSection(int Width, int Height){
+    // todo free
+
+    if(BitmapHandle){
+        DeleteObject(BitmapHandle);
+    } 
+    if (!BitmapDeviceContext) {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext, &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0,
+        0
+    );
+}
+
+
+internal void
+WinUpdateWindow(HDC DeviceContext, int x, int y, int w, int h){
+    StretchDIBits(
+        DeviceContext,
+        x, y, w, h,
+        x, y, w, h,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS,
+        SRCCOPY // DWORD dwRop
+    );
+}
+
+LRESULT CALLBACK WinMainWindowCallback(
     HWND Window,
     UINT Message,
     WPARAM WParam,
@@ -12,26 +64,33 @@ LRESULT CALLBACK MainWindowCallback(
     switch(Message) {
         case WM_SIZE:
         {
-        OutputDebugStringA("RESIZE\n");
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            WinResizeDIBSection(
+                Width, Height
+            );
+            OutputDebugStringA("RESIZE\n");
         } break;
-        case WM_DESTROY: {} break;
-        case WM_CLOSE: {} break;
+        case WM_DESTROY: {
+            OutputDebugStringA("DESTROY\n");
+            Running = false;
+        } break;
+        case WM_CLOSE: {
+            OutputDebugStringA("CLOSE\n");
+            Running = false;
+        } break;
         case WM_ACTIVATEAPP: {} break;
         case WM_PAINT: {
             PAINTSTRUCT Paint;
-
             HDC DevideContext = BeginPaint(Window, &Paint);
             int X = Paint.rcPaint.left;
             int w = Paint.rcPaint.right - X;
             int Y = Paint.rcPaint.top;
             int h = Paint.rcPaint.bottom - Y;
-            static DWORD Operation = BLACKNESS;
-            PatBlt(DevideContext, X, Y, w, h, Operation);
-            if (Operation == BLACKNESS) {
-                Operation = WHITENESS;
-            } else {
-                Operation = BLACKNESS;
-            }
+            local_persist DWORD Operation = BLACKNESS;
+            WinUpdateWindow(DevideContext, X, Y, w, h);
             EndPaint(Window, &Paint);
         } break;
         default: {
@@ -54,7 +113,7 @@ int CALLBACK WinMain(
     WNDCLASS WindowClass = {};
     const wchar_t CLASS_NAME[]  = L"Sample Window Class";
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.lpfnWndProc = WinMainWindowCallback;
     WindowClass.hInstance = Instance;
     WindowClass.lpszClassName = CLASS_NAME;
     
@@ -75,8 +134,9 @@ int CALLBACK WinMain(
             0
         );
         if (WindowHandle) {
-
-            for (;;) {
+            Running = true;
+            while (Running)
+            {
                 MSG Message = {};
                 BOOL Result = GetMessage(&Message, 0, 0, 0);
                 if (Result > 0) 
